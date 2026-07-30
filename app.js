72420f8e735a2d5b7bff7f58730c1706064e06d2
Guilherme Romera <g.romera2018@icloud.com>
2026-07-30 20:24:23 -0300

    console.log

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 alert ("Boas vindas ao jogo do número secreto");
 
 let numeroSecreto = 5;
+console.log("Número secreto: " + numeroSecreto);
 
 let chute = prompt("Digite um número entre 0 e 10");
 
 if (chute == numeroSecreto) {
-    console.log("Você acertou!");
+    alert(`Você acertou! ${numeroSecreto} é o número secreto!`);
+} else {
+    alert("Você errou!")
 }
