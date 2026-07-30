7a18ee23f89e0bb6c0ae86d2f0f12f127c90468b
Guilherme Romera <g.romera2018@icloud.com>
2026-07-30 19:34:08 -0300

    project initial

---
+++ app.js
@@ -0,0 +1,9 @@
+alert ("Boas vindas ao jogo do número secreto");
+
+let numeroSecreto = 5;
+
+let chute = prompt("Digite um número entre 0 e 10");
+
+if (chute == numeroSecreto) {
+    console.log("Você acertou!");
+}
